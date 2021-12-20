8651006d477f7b7d5c0977766978bf849348dccd
gymcoding <bruce.lean17@gmail.com>
2021-12-20 12:11:22 +0900

    Add load, defer, async

---
+++ lectures/3_javascript/script.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', function () {
+  alert('Hello World!');
+});
+// window.onload = function () {
+//   alert('onload');
+//   // let btn = document.querySelector('#btn');
+//   // btn.addEventListener('click', function () {
+//   //   alert('Hello World!');
+//   // });
+// };
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   alert('DOMContentLoaded');
+//   // let btn = document.querySelector('#btn');
+//   // btn.addEventListener('click', function () {
+//   //   alert('Hello World!');
+//   // });
+// });
